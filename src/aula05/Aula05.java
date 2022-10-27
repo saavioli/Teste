5a8b8311f42004f21a00983711ff09848eeed29a
Igor Savioli <igortestasavioli@hotmail.com>
2022-10-27 19:42:21 -0300

    commit teste

---
+++ src/aula05/Aula05.java
@@ -0,0 +1,12 @@
+package aula05;
+public class Aula05 {
+
+    public static void main(String[] args) {
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Igor");
+        p1.abrirConta("CC");
+        p1.estadoAtual();
+    }
+    
+}
